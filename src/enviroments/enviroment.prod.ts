562917b9832bc305967bcaa38526f1cca8410993
jdiegomoreno8 <54607355+jdiegomoreno8@users.noreply.github.com>
2025-11-21 11:31:03 -0500

    Delete src/enviroments/enviroment.prod.ts

--- src/enviroments/enviroment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.miapp.com', // 🚀 Cambia esto por tu URL real en producción
-  apiUrlCitas: 'http://127.0.0.1:8000/citas',
-  googleMapsApiKey: 'AIzaSyDF-4nItghEwCXm3FuRVGijRCHODyYTngo'
-};
